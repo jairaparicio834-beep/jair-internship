d935b55e9e5e1797291d881dab5dff99b3285c67
Jair Aparicio <jairaparicio834@gmail.com>
2026-03-18 10:52:29 -0700

    Added a skeleton loading state

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -4,26 +4,44 @@ import CollectionInfo from "../components/collection/CollectionInfo";
 import CollectionItems from "../components/collection/CollectionItems";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import CollectionPageSkeleton from "../components/ui/CollectionPageSkeleton";
 export default function CollectionPage() {
   const { id } = useParams()
   const [user, setUser] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   async function fetchCollection() {
-    const { data } = await axios.get(`https://remote-internship-api-production.up.railway.app/collection/${id}`)
-    const userData = data.data;
-    setUser(userData)
+    try {
+      const { data } = await axios.get(`https://remote-internship-api-production.up.railway.app/collection/${id}`)
+      const userData = data.data;
+      setUser(userData)
+      setLoading(false)
+    }
+    finally {
+      setLoading(false)
+    }
   }
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    setLoading(true)
     fetchCollection()
   }, [id]);
 
   return (
     <>
-      <CollectionHeader user={user} />
-      <CollectionInfo user={user} />
-      <CollectionItems user={user} />
+      {
+        loading ?
+          <>
+            <CollectionPageSkeleton />
+          </>
+          : <>
+            <CollectionHeader user={user} />
+            <CollectionInfo user={user} />
+            <CollectionItems user={user} />
+          </>
+      }
+
     </>
   );
 }
