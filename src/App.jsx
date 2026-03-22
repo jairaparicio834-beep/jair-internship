ee3a701611249b47f7e24af2956b9fe2517827df
Jair Aparicio <jairaparicio834@gmail.com>
2026-03-22 12:28:55 -0700

    Added dynamic route on userPage

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,26 @@ import HomePage from "./pages/HomePage";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ItemPage from "./pages/ItemPage";
 import UserPage from "./pages/UserPage";
-<<<<<<< HEAD
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 function App() {
-<<<<<<< HEAD
-
-=======
   useEffect(() => {
     AOS.init({
       duration: 800,
       offset: 200,
     });
   }, []);
->>>>>>> jair-popular-collection
-=======
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import { useEffect } from "react";
-function App() {
-  useEffect(() => {
-    AOS.init({
-      duration: 800,  // animation duration in ms
-      offset: 200,
-    })
-  }, [])
->>>>>>> jair-trendingNFTs
   return (
     <Router>
       <Nav />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/collections" element={<CollectionsPage />} />
-<<<<<<< HEAD
-=======
-        <Route path="/collection" element={<CollectionPage />} />
-        <Route path="/item" element={<ItemPage />} />
-        <Route path="/user/:id" element={<UserPage />} />
->>>>>>> jair-user-details
         <Route path="/collection/:id" element={<CollectionPage />} />
         <Route path="/item/:id" element={<ItemPage />} />
-        <Route path="/user" element={<UserPage />} />
+        <Route path="/user/:id" element={<UserPage />} />
       </Routes>
       <Footer />
     </Router>
