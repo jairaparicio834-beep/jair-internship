ae9b88ffc185c0cef75c10f2059a82c564a54a59
Jair Aparicio <jairaparicio834@gmail.com>
2026-03-22 11:23:23 -0700

    Added AOS fade up into popularCollections

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import HomePage from "./pages/HomePage";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import ItemPage from "./pages/ItemPage";
 import UserPage from "./pages/UserPage";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      offset: 200,
+    });
+  }, []);
   return (
     <Router>
       <Nav />
